1a77cadad7e96247f8fad31955d892bf49379fad
Your Name <you@example.com>
2021-03-21 04:23:58 +0000

    sucessful helloword

--- helloword.m
+++ helloword.m
@@ -1,7 +1,17 @@
 clc;
 clear;
-% setup;
-iBatch=2;
+addpath(genpath(pwd));
+
+set(groot, 'defaulttextinterpreter', 'latex');
+set(groot, 'defaultaxesticklabelinterpreter', 'latex');
+set(groot, 'defaultaxesfontname', 'latex');
+set(groot, 'defaultlegendinterpreter', 'latex');
+set(groot, 'defaultlinelinewidth', 2);
+set(groot, 'defaultstemlinewidth', 2);
+run('/rds/general/user/yz7219/home/Main/cvx/cvx_setup.m');
+iBatch=str2double(getenv('PBS_ARRAY_INDEX'));
+%disp(iBatch);
+%rng(iBatch);
 % print('hello world');
 m = 16; n = 8;
 A = randn(m,n);
@@ -10,6 +20,6 @@ cvx_begin quiet
     variable x(n)
     minimize( norm(A*x-b) )
 cvx_end
-save(sprintf('data/data_%iBatch.mat', iBatch));
+save(sprintf('data/data_%d.mat', iBatch));
 
 
